762aeb6f455cbab1061408a788e82c4714a87d43
1SamuelA <1300870@abertay.ac.uk>
2017-04-12 14:26:35 +0100

    Game Works Now to add Towers

---
+++ TD_PATH_KINECT/Assets/TerrainGen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<MeshFilter> ().mesh = GameObject.Find ("_SandBoxData").GetComponent<SandBoxData> ().ARS_Data.mesh;
+		GetComponent<MeshCollider> ().sharedMesh = GetComponent<MeshFilter> ().mesh;
+		GameObject.Find ("A_").GetComponent<AstarPath> ().Scan ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
